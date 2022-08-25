46294a6284f27d2792a3d69536f085980f6e2221
FiLLkrd <ma.antonoff2012@yandex.ru>
2022-08-26 02:44:47 +0300

    1

--- script/index.js
+++ script/index.js
@@ -1,32 +1,3 @@
-//массив данных для создания стандартных карточек при загрузке страницы
-
-const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
-
 //открытие модального окна редактирования имени и деятельности пользователя
 
 const popup = document.querySelector('.popup');
@@ -50,44 +21,42 @@ const popupJob = document.querySelector('.popup__input_type_job');
 //Переменные для работы с модальным окном добавления новой карточки
 
 const formCard = document.querySelector('.popup__form_type_add-card');
-
 const popupCardName = document.querySelector('.popup__input_type_title');
 const popupCardLink = document.querySelector('.popup__input_type_link');
 
-//Переменные для добавления набора карточек из массива на страницу при загрузке (стандартные карточки)
+//Переменные для добавления набора карточек из массива на страницу при загрузке 
 
 const elementsContainer = document.querySelector('.elements');
 const templateContainer = document.querySelector('#template-card').content;
 const elementCards = document.querySelector('.elements');
 const elementCard = document.createElement('li')
 
-const image = document.querySelector('.element__image');
+//Переменные для открытия фото на весь экран
+
+const imageFull = document.querySelector('.popup__fullscreen');
+const cap = document.querySelector('.popup__cap');
 const popupFull = document.querySelector('.popup_type_full');
+const buttonImageClose = document.querySelector('.popup__close-button_type_full');
 
-
-
-
-
-
-  
-const elementInfo = initialCards.map(function (item) {
-      return {
-          name: item.name,
-          link: item.link
-      };
-  });
+//Функция перебора массива
 
 function render() {
-    elementInfo.forEach(renderCard);
+    initialCards.forEach(renderCard);
   }
 
+//Функция вывода карточек на страницу
+
   function renderCard({ name, link }) {
-    const templateElement = templateContainer.querySelector(".element").cloneNode(true);
+    const templateElement = templateContainer.querySelector('.element').cloneNode(true);
+    const image = templateElement.querySelector('.element__image');
 
-      templateElement.querySelector(".element__title").textContent = name;
-      templateElement.querySelector(".element__image").src = link;
+      
+      image.src = link;
+      image.alt = name;
+      templateElement.querySelector('.element__title').textContent = name;
 
 
+      image.addEventListener('click', openPopupFullListen);
 
             //Проставляем лайк, смена прозрачного лайка на закрашенный
   
@@ -107,18 +76,7 @@ function render() {
 
   render();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    const nameCard = popupCardName.value;
-    const linkCard = popupCardLink.value;
-    popupCardName.value = '';
-    popupCardLink.value = '';
-
-    renderCard(nameCard, linkCard);
-    closePopupAdd();
-
-    }
+//Функция открытия фотографии на весь экран
 
     function openPopupFull () {
         popupFull.classList.add('popup_opened');
@@ -133,12 +91,21 @@ function closePopupAdd() {
 
 //Функция открытия модального окна для создания карточки
 
-
 function openPopupAdd() {
-    popupAdd.classList.add('popup_opened');
-    
-    
+    popupAdd.classList.add('popup_opened'); 
 }
+
+//Функция добавления карточки новой, через форму
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    renderCard({
+        name: popupCardName.value,
+        link: popupCardLink.value});
+
+    closePopupAdd();
+    }
 
 //Функция закрытия модального окна для редактирования пользователя
 
@@ -148,17 +115,14 @@ function closePopup() {
 
   //Функция открытия модального окна для редактирования пользователя
 
-
 function openPopup() {
     popup.classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupJob.value = profileJob.textContent;
 }
-
-
 //Функция изменения данных о пользователе "сабмит" через модальное окно
 
-function formSubmitHandler(e) {
+function editSubmitHandler(e) {
     e.preventDefault();
 
     let popupNameInput = popupName.value;
@@ -170,19 +134,30 @@ function formSubmitHandler(e) {
     closePopup();
 }
 
+function openPopupFullImage(src, figcaption) {
+    popupFull.classList.add('popup_opened');
+
+    imageFull.setAttribute('src', src);
+    cap.textContent = figcaption;
+ }
+ 
+ function openPopupFullListen(evt) {
+    openPopupFullImage(evt.target.getAttribute('src'), evt.target.getAttribute('alt'));
+ }
+ 
+ function closePopupImage() {
+    popupFull.classList.remove('popup_opened');
+ }
+
+//Слушатели на кнопках и формах
+
 formCard.addEventListener('submit', handleSubmit);
-
-profileForm.addEventListener('submit', formSubmitHandler);
-
+profileForm.addEventListener('submit', editSubmitHandler);
 buttonEdit.addEventListener('click', openPopup);
-
 buttonEditClose.addEventListener('click', closePopup);
-
 buttonAdd.addEventListener('click', openPopupAdd)
-
 buttonAddClose.addEventListener('click', closePopupAdd);
-
-image.addEventListener('click', openPopupFull);
+buttonImageClose.addEventListener('click', closePopupImage);
 
 
 
